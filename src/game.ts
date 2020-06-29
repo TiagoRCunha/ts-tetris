33200dd095cb354cd414829300ea6dc1d421ddaa
Tiago Rodrigues Cunha <tiago.r.c.sn@gmail.com>
2020-06-29 15:40:04 -0300

    add state

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,30 @@
-export default class Game {
+interface IScreen {
+  width: number | string;
+  height: number | string;
+}
+
+interface IPlayer {}
+
+interface IState {
+  player: IPlayer;
+  lines: number;
+  screen: IScreen;
+}
+
+interface IGame {
+  state: IState;
+}
+
+export default class Game implements IGame {
+  state = {
+    player: {},
+    lines: 0,
+    screen: {
+      width: 280,
+      height: 450,
+    },
+  };
+
   constructor() {
     this.test();
   }
