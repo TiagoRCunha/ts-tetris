c6c7f73c6a24b7edd3955e79650a3ea8f2bb7c6b
Tiago Rodrigues Cunha <tiago.r.c.sn@gmail.com>
2022-03-20 20:14:52 -0300

    fix dev server on client and server

---
+++ service/webpack.config.js
@@ -0,0 +1,58 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+const webpack = require("webpack");
+const nodeExternals = require('webpack-node-externals');
+const path = require("path");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  externals: [nodeExternals()],
+  externalsPresets: {
+    node: true // in order to ignore built-in modules like path, fs, etc. 
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    fallback: {
+      "stream": false,
+      "http": require.resolve("http")
+    }
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
