7044a71102fe2a4c5bc290a68e103a640eed356c
Gustavo Christ <gustavo.olichrist@gmail.com>
2022-06-09 10:14:29 -0300

    button how to play added

--- web/public/script/control.js
+++ web/public/script/control.js
@@ -1,4 +1,16 @@
 let startBtn = document.querySelector('.js-start-btn');
+let htpButton = document.querySelector('.how-to-play-button')
+
+
+
+htpButton.addEventListener("click", function(e){
+    Swal.fire({
+        template: '#modal'
+      })
+})
+
+
+
 
 // startBtn.addEventListener('click',function(e){
 //     alert(e.target);
